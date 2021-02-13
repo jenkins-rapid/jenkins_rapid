a648a97758220a7773c65a9f7466f72f5cbecfe0
Sid <sidwho@gmail.com>
2021-02-14 09:09:44 +1100

    Version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def long_description():
 
 
 setup(name='jenkins_rapid',
-      version='0.3.9',
+      version='0.3.10',
       description='A simple tool to rapidly create and debug jenkins pipelines',
       long_description=long_description(),
       long_description_content_type='text/markdown',
