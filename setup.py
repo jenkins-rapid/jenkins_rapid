c90c12d4738831cf843cef03f2378756c22262d3
Sid <sidwho@gmail.com>
2021-03-04 15:33:58 +1100

    Clear screen on startup

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def long_description():
 
 
 setup(name='jenkins_rapid',
-      version='0.4.5',
+      version='0.4.6',
       description='A simple tool to rapidly create and debug jenkins pipelines',
       long_description=long_description(),
       long_description_content_type='text/markdown',
